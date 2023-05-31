9bc31148599019d2aa434338a122fa48b1bbbc0f
Ariel Moreshet <amoreshe@g.jct.ac.il>
2023-05-31 16:18:26 +0300

    first comment

---
+++ Controllers/BranchTypeController.cs
@@ -0,0 +1,47 @@
+﻿//using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewEngines;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Umbraco.Cms.Core;
+using Umbraco.Cms.Core.Models.PublishedContent;
+using Umbraco.Cms.Core.PublishedCache;
+using Umbraco.Cms.Core.Services;
+using Umbraco.Cms.Core.Web;
+using Umbraco.Cms.Web.Common.Controllers;
+using Umbraco.Web.PublishedCache;
+
+
+namespace UmbracoProject.Controllers
+{
+    public class BranchTypeController : Controller
+    {
+        [HttpPost]
+        public ActionResult? Submit() => toJson();
+
+        [HttpPost]
+        private ActionResult? toJson()
+        {
+            IPublishedContentCache publishedContentCache = Umbraco.Web.Composing.Current.UmbracoContext.Content;
+            IPublishedContent ? page = publishedContentCache.GetByXPath("//branchType").FirstOrDefault();
+
+            if (page != null)
+            {
+                // Convert the data to JSON
+                var jsonData = new
+                {
+                    Property1 = page.Value<string>("BranchName"),
+                    Property2 = page.Value<int>("BranchId"),
+                    // Add more properties as needed
+                };
+
+                string jsonString = JsonConvert.SerializeObject(jsonData, Formatting.Indented);
+
+                // Save the JSON data to a file
+                string filePath = "path/to/your/file.json";
+                System.IO.File.WriteAllText(filePath, jsonString);
+            }
+            return Content("JSON file generated successfully!");
+        }
+    }
+}
---
+++ Models/BranchModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UmbracoProject.Models
+{
+    public class BranchModel
+    {
+        [Required]
+        public int BranchId { get; set; }
+        [Required]
+        [MaxLength(250)]
+        public string ? BranchName { get; set; }
+    }
+}
